4852ac893d90d9fc6302399b4a330f9228f71b78
Sung27 <shj_27@naver.com>
2024-07-09 17:33:08 +0900

    feat : [FE] loginpageUpdate

---
+++ js/login/joinPaly.js
@@ -0,0 +1,5 @@
+const modal = document.querySelector('.modal');
+const btnOpenModal=document.querySelector('.bt');
+btnOpenModal.addEventListener("click", ()=>{
+    modal.style.display="flex";
+});
