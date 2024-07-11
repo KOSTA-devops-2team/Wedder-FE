fe6296b07394d2ef969c7872852669a724256fa2
cherryJubilee <hyewon5026@gmail.com>
2024-07-11 14:22:56 +0900

    feat : [FE] 웨딩홀 상세페이지 - 견적계산기 1차 완성

--- js/weddingHall/weddingHallDetail.js
+++ js/weddingHall/weddingHallDetail.js
@@ -3,3 +3,10 @@ document.querySelectorAll(".time-list").forEach((item) => {
         item.classList.toggle("time-list-selected");
     });
 });
+
+document.querySelectorAll(".month-grid").forEach((item) => {
+    item.addEventListener("click", () => {
+        item.classList.remove("month-grid");
+        item.classList.toggle("month-grid-selected");
+    });
+});
