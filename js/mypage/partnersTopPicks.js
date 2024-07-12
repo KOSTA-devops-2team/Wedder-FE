c880c0a00a7836e1548c75ea4a6ee8ce70c53851
parkblacksheep <p7421772@naver.com>
2024-07-12 18:21:05 +0900

    feat : [FE] switch partnersTopPicks

---
+++ js/mypage/partnersTopPicks.js
@@ -0,0 +1,8 @@
+const Cards = document.querySelectorAll(".Card");
+const Cardhover=document.querySelector(".Card-hover");
+
+Cards.forEach(card =>{
+    card.addEventListener('mouseover ',function(){
+        this.style.
+    })
+})
