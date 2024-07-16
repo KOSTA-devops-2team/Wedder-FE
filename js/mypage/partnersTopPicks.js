5f5dff18ff1cd44004a081ba5838a65a9ad20261
parkblacksheep <p7421772@naver.com>
2024-07-16 20:16:25 +0900

    style : [FE} partnersTopPicks branch style change

--- js/mypage/partnersTopPicks.js
+++ js/mypage/partnersTopPicks.js
@@ -1,8 +1,43 @@
-const Cards = document.querySelectorAll(".Card");
-const Cardhover=document.querySelector(".Card-hover");
+function SliderBox1__init() {
+    const swiper = new Swiper(".slider-box-1 .swiper", {
+      // Optional parameters
+      loop: true,
+  
+      // If we need pagination
+      pagination: {
+        el: ".slider-box-1 .swiper-pagination"
+      },
+  
+      // Navigation arrows
+      navigation: {
+        nextEl: ".slider-box-1 .swiper-button-next",
+        prevEl: ".slider-box-1 .swiper-button-prev"
+      }
+    });
+  }
+  
+  function SliderBox2__init() {
+    const swiper = new Swiper(".slider-box-2 .swiper", {
+      // Optional parameters
+      loop: true,
+  
+      // If we need pagination
+      pagination: {
+        el: ".slider-box-2 .swiper-pagination"
+      },
+  
+      // Navigation arrows
+      navigation: {
+        nextEl: ".slider-box-2 .swiper-button-next",
+        prevEl: ".slider-box-2 .swiper-button-prev"
+      }
+    });
+  }
+  
+  
+  SliderBox1__init();
+  SliderBox2__init();
 
-Cards.forEach(card =>{
-    card.addEventListener('mouseover ',function(){
-        this.style.
-    })
-})
+
+
+  
