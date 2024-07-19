c287c781de0270e73ec3e5e871f2d8c9aedfedfd
JeongHwa518 <jeonghwa485@gmail.com>
2024-07-19 17:54:58 +0900

    feat : [FE] 스드메 예약 결제 페이지 1차 완성

--- js/reservation/reservationSelectCompany.js
+++ js/reservation/reservationSelectCompany.js
@@ -1,11 +1,12 @@
 import "../../css/reservation/reservationSelectCompany.css"
 
-const actBtn = document.querySelector(".tab.active")
-const btn = document.querySelector(".tab")
-const tagSearch = document.querySelector("#tag-search")
-const scheduleSearch = document.querySelector("#schedule-search")
+const btn = document.querySelectorAll(".tabs")
+const contents = document.querySelectorAll("#tab-contents")
+let activeCont = '';
+/* const tagSearch = document.querySelector("#tag-search")
+const scheduleSearch = document.querySelector("#schedule-search") */
 
-actBtn.addEventListener("click", () => {
+/* actBtn.addEventListener("click", () => {
     tagSearch.style.display = "block";
     scheduleSearch.style.display = "none";
     btn.classList.remvove("tab.active")
@@ -15,8 +16,22 @@ actBtn.addEventListener("click", () => {
 btn.addEventListener("click", () => {
     tagSearch.style.display = "none";
     scheduleSearch.style.display = "block";
-   /*  btn.style.color = "#CA1A5A";
-    btn.style.borderBottom = "2px solid #CA1A5A"; */   
+    btn.style.color = "#CA1A5A";
+    btn.style.borderBottom = "2px solid #CA1A5A";  
     btn.classList.add("tab.active")
     actBtn.classList.remove("tab.active")
-})
+}) */
+
+for(var i = 0; i < btn.length; i++) {
+    btn[i].querySelector('.tab').addEventListener('click', function(e){
+        e.preventDefault();
+        for(var j = 0; j < btn.length; j++) {
+            btn[j].classList.remove('.tab.active')
+
+            contents[j].style.display = 'none';
+        }
+        this.parentNode.classList.add('.tab.active');
+
+        activeCont = this.getAttribute('div');
+        document.querySelector(activeCont).style.display = 'block';
+})}
