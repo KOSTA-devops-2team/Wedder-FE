9c5cbf24350533579077828505779a369fcae6ec
Sung27 <shj_27@naver.com>
2024-07-23 18:11:45 +0900

    fix : [FE] alphabet bug

--- js/login/login.js
+++ js/login/login.js
@@ -9,23 +9,6 @@ let pas = document.querySelector('#Password');
 const loginbutton = document.querySelector('#continue');
 
 
-function showPopup() {
-     window.open("passfind.html", "a", "width=500, height=700, left=100,resizable=no, menuber=no, scrollbars=no "); 
-    }
-
-function usePopup() {
-window.open("uselaw.html", "a", "width=710, height=700, left=100,resizable=no, menuber=no, scrollbars=no "); 
-}
-
-
-function privacyPopup() {
-window.open("privacy.html", "a", "width=710, height=700, left=100,resizable=no, menuber=no, scrollbars=no "); 
-}
-
-
-function marketingPopup() {
-window.open("marketing.html", "a", "width=710, height=700, left=100,resizable=no, menuber=no, scrollbars=no "); 
-}
 
 
 
