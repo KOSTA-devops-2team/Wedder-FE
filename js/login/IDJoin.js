e7c3c068329fe200c5e85739a0b227edca8b03ae
Sung27 <shj_27@naver.com>
2024-07-24 17:26:33 +0900

    feat : [FE] 로그인생성js  및 패스워드 생성js

---
+++ js/login/IDJoin.js
@@ -0,0 +1,17 @@
+let id = document.querySelector("#idjoin");
+let btn = document.querySelector(".bt")
+const fil = /[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*/gi;
+id.addEventListener("keyup",()=>{
+  
+
+    if(fil.test(id.value)){
+        btn.style.backgroundColor ="black";
+        
+       
+    
+    }else if(!fil.test(id.value)) {
+        btn.style.backgroundColor ="#b1b1b1";
+       
+       
+    }
+});
