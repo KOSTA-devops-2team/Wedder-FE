90f12edd71f56e1d8219aeaeee369dc02647c107
JeongHwa518 <jeonghwa485@gmail.com>
2024-07-26 14:19:33 +0900

    chore : [FE] 견적함 페이지, 자바스크립트 추가

--- js/visitReservation/visitReserveInfo.js
+++ js/visitReservation/visitReserveInfo.js
@@ -3,38 +3,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const modal = document.querySelector(".modal");
     const modalBtn = document.querySelector(".btn-nxt");
     const closeBtn = document.querySelector(".cancel-btn");
+
   
     modalBtn.addEventListener("click", function() {
-        modal.classList.add("show")
-    })
+        modal.classList.add("show");
+        modal2.classList.remove("show2");
+    });
+
     closeBtn.addEventListener("click", function() {
-        modal.classList.remove("show")
-    })
+        modal.classList.remove("show");
+    });
 
     /* 예약 완료 모달창 */
     const modalNxt = document.querySelector(".check-btn");
     const modal2 = document.querySelector(".modal2");
 
     modalNxt.addEventListener("click", function() {
-        modal.classList.remove("show")
-        modal2.classList.add("show")    
+        modal.classList.remove("show");
+        modal2.classList.add("show2");
     })
 
-
-
-    // functions
-    /* function toggleModal() {
-      modal.classList.toggle("show");
-    } */
-  
-    /* window.addEventListener("click", function(event) {
-      // 모달의 검은색 배경 부분이 클릭된 경우 닫히도록 하는 코드
-      if (event.target === modal) {
-        modal.classList.remove("show")
-    }); */
-    window.onclick = function (event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  };
+    /* 외부 클릭 시 모달창 닫기 */ 
+    window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+            modal.classList.remove("show");
+        }
+        /* if (event.target === modal2) {
+            modal2.classList.remove("show2");
+        } */
+    });
   });
