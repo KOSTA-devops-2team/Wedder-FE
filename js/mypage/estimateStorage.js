caa1cdd5d12ff5f126b6782432f74e7a9d5b3543
JeongHwa518 <jeonghwa485@gmail.com>
2024-08-06 16:56:24 +0900

    feat : [FE] 스드메 상세 페이지 추가

--- js/mypage/estimateStorage.js
+++ js/mypage/estimateStorage.js
@@ -2,19 +2,38 @@
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.querySelector(".modal");
     const closeBtn = document.querySelector(".modal-close-btn");
-    const modalOn = document.querySelector("#icon");
 
-    modalOn.addEventListener("click", function() {
-        modal.classList.add("show");
+    document.querySelectorAll("#icon").forEach((icon) => {
+        icon.addEventListener("click", () => {
+            modal.classList.add("show");
+        })
     });
-
+    
     closeBtn.addEventListener("click", function() {
         modal.classList.remove("show");
     });
 
+    /* 삭제 모달창 */
+    const modal2 = document.querySelector(".modal2");
+    const closeBtn2 = document.querySelector(".cancel-btn");
+
+    document.querySelectorAll(".storage-delete").forEach((item) => {
+        item.addEventListener("click", () => {
+            modal2.classList.add("show2");
+        })
+    });
+
+    closeBtn2.addEventListener("click", function() {
+        modal2.classList.remove("show2")
+    }) 
+
+    /* 외부 클릭 시 모달창 닫기 */
     window.addEventListener("click", function(event) {
         if (event.target === modal) {
             modal.classList.remove("show");
+        }
+        if (event.target === modal2) {
+            modal2.classList.remove("show2");
         }
     });
   });
